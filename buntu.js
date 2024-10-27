9c479579301003406ff98be55fc5af2d22baacc8
Isayah <218007520@tut4life.ac.za>
2024-10-27 15:47:27 +0200

    i have udated all my files with modification and fixing issues

--- buntu.js
+++ buntu.js
@@ -156,3 +156,11 @@ function prevTarget(serviceId) {
 // whatever login
 
 
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) { // Adjust this value as needed
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+})
